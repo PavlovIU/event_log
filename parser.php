009c773cfd9cde258042cef4c368012cbf8ff5f0
Ilya Pavlov <ipavlov@simtechdev.com>
2020-02-09 16:07:55 +0400

    T-01 : Correct README, add php-script, add event.log example

---
+++ parser.php
@@ -0,0 +1,34 @@
+<?php
+
+$file = 'event.log';
+$result = [];
+
+if ($lines = fopen($file, 'r')) {
+	$i = 1;
+	$pattern = '/\[(.+?)\]\ ([^ ]+)/';
+	while (!feof($lines)) {
+		if ($line = trim(fgets($lines))) {
+			if (preg_match($pattern, $line, $matches)) {
+				if (!empty($matches)) {
+					list($line, $time, $status) = $matches;
+					$time = explode(" ", $time);
+					list($date, $hour_minuts) = $time;
+					$hour_minuts = substr($hour_minuts, 0, -3);
+					if (!array_key_exists($date, $result)) {
+						$result[$date] = [];
+					}
+					$status = strtolower($status);
+					if ($status == "nok") {
+						if (!array_key_exists($hour_minuts, $result[$date])) {
+							$result[$date][$hour_minuts] = 1;
+						} else {
+							$result[$date][$hour_minuts]++;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+print_r($result);
